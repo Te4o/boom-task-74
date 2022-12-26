dcd59bea8afbbfff75ec3eb4dbf2e1e1ac6bfd07
Stoyan Dimitrov <stoyan.jordanov94@gmail.com>
2022-12-26 22:15:31 +0200

    task-74: number validator

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import "./App.css";
+import React, { useState, useMemo } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+
+  const isValid = useMemo(() => {
+    //const num = parseInt(text, 10);
+    const rgx = new RegExp("^[0-9]+$");
+
+    return rgx.test(text);
+    //return !isNaN(num);
+  }, [text]);
+
   return (
     <div className="App">
-     <div className="control has-icons-right">
+      <div className="control has-icons-right">
         <input
           className="input is-large"
           type="text"
           placeholder="Enter number..."
-
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
         <span className="icon is-small is-right">
-          <i className="fas fa-times" />
-        </span> 
+          {/* <i className="fas fa-times" /> */}
+          <i className={`fas ${isValid ? "fa-check" : "fa-times"}`} />
+        </span>
       </div>
     </div>
   );
